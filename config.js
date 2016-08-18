c40b0d514335ac62587e39e527e10dd494b5de73
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-08-18 12:04:18 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+
+var config = {
+	db: {
+		host: 'localhost',
+		user: 'root',
+		password: '', //Remove this before commit
+		database: 'tagger'
+	},
+	secret: '' //Remove this before commit
+}
+
+module.exports = config;
