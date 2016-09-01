ebb8758430468169010f7f84a4baf8b33cd9b948
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-09-01 10:47:26 +0530

    Add CORS headers

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET");
+  next();
+});
+
 app.use('/api/login', authenticator);
 app.use('/api/user', user);
 app.use('/api/product', product);
