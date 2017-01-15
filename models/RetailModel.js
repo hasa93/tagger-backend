552ffdf3353ae02b4f5fef7a039eba5157d485b6
Hasanga Somaratne <hasanga.charaka@gmail.com>
2017-01-16 00:18:23 +0530

    Include total sum in favorite products query

--- models/RetailModel.js
+++ models/RetailModel.js
@@ -171,7 +171,8 @@ exports.getSalesById = function(startDate, endDate, prodId, callBack){
 
 exports.getMostSoldProducts = function(callBack){
 	var sql = "SELECT products.prod_name AS name,\
-			   SUM(sales_records.qty) AS qty\
+			   SUM(sales_records.qty) AS qty,\
+			   (sales_records.qty * products.unit_price) AS total\
 			   FROM products INNER JOIN sales_records\
 			   ON products.prod_id = sales_records.product_id WHERE products.discontinued IS FALSE\
 			   GROUP BY (sales_records.product_id) ORDER BY SUM(sales_records.qty)\
